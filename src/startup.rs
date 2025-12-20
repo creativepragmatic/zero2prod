013bec9e06a184e0af7bdffbfb057aa7f8a61b0e
Creative Pragmatic <creativepragmatic@177726.no-reply.drupal.org>
2025-12-20 17:41:53 -0500

    Chapter 3

---
+++ src/startup.rs
@@ -0,0 +1,26 @@
+use crate::routes::health_check;
+use crate::routes::subscribe;
+use actix_web::{App, HttpServer, dev::Server, web};
+use sqlx::PgPool;
+use std::net::TcpListener;
+
+// Notice the different signature!
+// We return `Server` on the happy path and we dropped the `async` keyword
+// We have no .await call, so it is not needed anymore.
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    // Wrap the pool using web::Data, which boils down to an Arc smart pointer
+    let db_pool = web::Data::new(db_pool);
+
+    // Capture `connection` from the surrounding environment
+    let server = HttpServer::new(move || {
+        App::new()
+            .route("/health_check", web::get().to(health_check))
+            .route("/subscriptions", web::post().to(subscribe))
+            .app_data(db_pool.clone())
+    })
+    .listen(listener)?
+    .run();
+
+    // No .await here!
+    Ok(server)
+}
